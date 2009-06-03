aacefbcf4585cc3c8fa6183e6b6191eb2df1ec92
dglinenko <dglinenko@6faa146a-fa50-0410-b6d0-e91f4ee16382>
2009-06-03 00:26:02 +0000

    changed version.

---
+++ src/net/codemate/Main.java
@@ -0,0 +1,29 @@
+/**
+ * @(#) Main.java;
+ * <p/>
+ * Created on Jul 4, 2008
+ * AUTHOR    ** Danil Glinenko
+ * EMAIL     ** dglinenko@gmail.com
+ * <p/>
+ * /**
+ *
+ * Copyright (C) 2008 Danil Glinenko
+ */
+
+package net.codemate;
+
+
+public class Main {
+    private static final String VERSION = "0.9.2";
+
+    private Main() {
+    }
+
+    public static void main(final String[] args) {
+        for (final String arg : args) {
+            if (arg.equals("-v")){
+                System.out.println("DbAssert " + VERSION);
+            }
+        }
+   }
+}
