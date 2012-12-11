c607163e0031d875dfdc86ef707cf12ad2a79a16
dglinenko@gmail.com <dglinenko@gmail.com@6faa146a-fa50-0410-b6d0-e91f4ee16382>
2012-12-11 20:12:20 +0000

    extended schema for HSQL db

--- test/net/codemate/HSqlSourceSchema.sql
+++ test/net/codemate/HSqlSourceSchema.sql
@@ -1,7 +1,36 @@
 DROP TABLE customers IF EXISTS;
+DROP TABLE IF EXISTS test_table;
+DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS domains;
 
 CREATE TABLE customers
 (
   id int NOT NULL,
-  name character varying
+  name character varying,
+  date_cr date,
+  number integer
+);
+
+
+CREATE TABLE events
+(
+  id int NOT NULL,
+  name character varying,
+  login_name character varying
+);
+
+CREATE TABLE test_table
+(
+  name character varying,
+  id int NOT NULL,
+  CONSTRAINT test_table_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE domains
+(
+    id int NOT NULL,
+    name character varying,
+    parent_id integer not null,
+    PRIMARY KEY(id),
+    FOREIGN KEY (parent_id) REFERENCES domains(id)
 );
